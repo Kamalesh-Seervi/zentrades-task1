f2a9bb85b9bdb8b96120739f698e6a7bb8e50c84
kd14 <kd14@ford.com>
2024-01-08 14:24:46 +0530

    task1

---
+++ task1.py
@@ -0,0 +1,31 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def display_data():
+    # Fetch JSON data from the API
+    url = "https://s3.amazonaws.com/open-to-cors/assignment.json"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        products = data.get('products', {})
+
+        if products:
+            product_list = list(products.values())
+            sorted_products = sorted(product_list, key=lambda x: int(x['popularity']), reverse=True)
+
+            result = ""
+            for product in sorted_products:
+                result += f"Title: {product['title']}<br>"
+                result += f"Price: {product['price']}<br>"
+                result += f"Popularity: {product['popularity']}<br>"
+                result += f"Subcategory: {product['subcategory']}<br>"
+                result += "=" * 30 + "<br>"
+
+            return result
+
+if __name__ == '__main__':
+    app.run(debug=True)
